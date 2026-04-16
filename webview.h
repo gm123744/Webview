b53d682c489cf5edcb672182745bc026ca1a5945
GM11 <skatamer53@gmail.com>
2026-04-16 20:13:52 +0300

    Refactor webview.h for versioning and updates

--- webview.h
+++ webview.h
@@ -2,6 +2,10 @@
 #define WEBVIEW_H
 
 #include <stdbool.h>
+#include <limits.h>
+
+//====================VERSION 2.0====================//
+#if defined(JVERSION_2_0)
 
 #ifdef __cplusplus
 extern "C" {
@@ -9,7 +13,6 @@ extern "C" {
 
 #define URL 300
 #define DOCUMENT 900
-
 
 typedef struct {
     int width;
@@ -33,6 +36,61 @@ typedef struct {
 } JDisplayContent;
 
 typedef enum {
+    JSIGINT = 2,
+    JSIGKILL = 9,
+    JSIGTERM = 15
+} JSysSignal;
+
+int CreateContext(JWindowSettings *settings, JDisplayContent *content, int ChromiumLogs, bool ErrorS);
+void DestroyContext(int pid, int debug, int signal);
+void JwebviewTerminate(int pid);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+//==================== VERSION 2.5 ====================//
+#if defined(JVERSION_2_5)
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define URL 300
+#define DOCUMENT 900
+
+typedef struct {
+    int width;
+    int height;
+} JWindowSize;
+
+typedef struct {
+    JWindowSize WindowSize;
+    int Zoom;
+    
+    //flags
+    unsigned int AddressBar;
+    unsigned int FullScreen;
+    unsigned int Incognito;
+    unsigned int KioskMode;
+    unsigned int GPU;
+    unsigned int ChromiumLogs;
+    unsigned int JWLOG;
+    unsigned int DisableGoogleFeatures;
+    unsigned int ForceDarkMode;
+    unsigned int HideScrollBars;
+    unsigned int DisableDevTools;
+
+} JWindowSettings;
+
+typedef struct {
+    int Ctype;
+    char buffer[4096];
+} JDisplayContent;
+
+typedef enum {
     RELOADWINDOW = 0,
     BACK = 1,
     FORWARD = 2,
@@ -40,36 +98,19 @@ typedef enum {
 } JSignal;
 
 typedef enum {
-    ADDRESSBAR = 1,
-    FULLSCREEN = 1,
-    INCOGNITO = 1,
-    KIOSKMODE = 1,
-    ENABLE_GPU = 1,
-    CHROMIUM_LOGS =1,
-    DEBUG = 1,
-    JEB_LOG = true,
-    NO_ADDRESSBAR = 0,
-    NO_FULLSCREEN = 0,
-    NO_INCOGNITO = 0,
-    NO_KIOSKMODE = 0,
-    DISABLE_GPU = 0,
-    NO_CHROMIUM_LOGS = 0,
-    NO_DEBUG = 0,
-    NO_WEBVIEW_LOG = false
-} JEflags;
-
-typedef enum {
     JSIGINT = 2,
     JSIGKILL = 9,
     JSIGTERM = 15
 } JSysSignal;
 
-
-int CreateContext(JWindowSettings *settings, JDisplayContent *content, int ChromiumLogs ,bool ErrorS);
-void DestroyContext(int pid,int debug, int signal);
+int CreateContext(JWindowSettings *settings, JDisplayContent *content);
+void DestroyContext(int pid, int signal);
 void JwebviewTerminate(int pid);
+
 #ifdef __cplusplus
 }
+#endif
+
 #endif
 
 #endif
